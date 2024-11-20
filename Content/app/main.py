123bf8090c9d8a163394a7a73a284b0632a1d7ba
jojopko <konstantin.serez@gmail.com>
2024-11-20 14:38:20 +0300

    [DEV-25] Added root router and DEBUG env var for config

--- Content/app/main.py
+++ Content/app/main.py
@@ -1,21 +1,15 @@
 from fastapi import Depends, FastAPI
-from pydantic import BaseModel
 
+from app.config import config
 from app.videos.views import router as videos_router
-
-class BaseMessage(BaseModel):
-    status: int
-    message: str
+from app.views import router as root_router
 
 
 app = FastAPI(
+    title='Content Service',
+    debug=config.debug,
     root_path='/content'
 )
 
-
-@app.get("/")
-async def health_check():
-    return BaseMessage(status=200, message='Ok')
-
-
+app.include_router(root_router, prefix="")
 app.include_router(videos_router, prefix='/videos')
