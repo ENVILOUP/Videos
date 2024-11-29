799f40a919118f42e6b5925545e26815e6388ee4
BloodyAngel22 <NanoViper222@yandex.ru>
2024-11-29 11:37:54 +0300

    [DEV-23] Add Health Check. Code Refactoring in Program.cs

--- Authorization/app/Program.cs
+++ Authorization/app/Program.cs
@@ -1,106 +1,23 @@
-using app.Models;
-using app.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
+using app;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = Environment.GetEnvironmentVariable("DATABASE__CONNECTIONSTRING")
+					   ?? builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddOpenApi();
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(opt => {
-	opt.SwaggerDoc("v1", new() { Title = "Auth API", Version = "v1" });
-	opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-	{
-		In = ParameterLocation.Header,
-		Description = "Please enter token",
-		Name = "Authorization",
-		Type = SecuritySchemeType.Http,
-		BearerFormat = "JWT",
-		Scheme = "Bearer"
-	});
-	opt.AddSecurityRequirement(new OpenApiSecurityRequirement
-	{
-		{
-			new OpenApiSecurityScheme
-			{
-				Reference = new OpenApiReference
-				{
-					Type = ReferenceType.SecurityScheme,
-					Id = "Bearer"
-				}
-			},
-			new string[] {}
-		}
-	});
-});
-
-builder.Services.AddScoped<JWTService>();
-
-builder.Services.AddDbContext<AppDbContext>(opt =>
-{
-	var connectionString = Environment.GetEnvironmentVariable("DATABASE__CONNECTIONSTRING")
-						   ?? builder.Configuration.GetConnectionString("DefaultConnection");
-	opt.UseNpgsql(connectionString);
-});
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(opt => {
-	opt.Password.RequireDigit = false;
-	opt.Password.RequiredLength = 3;
-	opt.Password.RequireLowercase = false;
-	opt.Password.RequireUppercase = false;
-	opt.Password.RequireNonAlphanumeric = false;
-})
-	.AddEntityFrameworkStores<AppDbContext>()
-	.AddDefaultTokenProviders();
-
-var key = builder.Configuration["JWT:Key"];
-builder.Services.AddAuthentication(opt =>
-{
-	opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-	opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(opt =>
-{
-	opt.TokenValidationParameters = new TokenValidationParameters
-	{
-		ValidateIssuer = true,
-		ValidateAudience = true,
-		ValidateLifetime = true,
-		ValidateIssuerSigningKey = true,
-		ValidIssuer = builder.Configuration["JWT:Issuer"],
-		ValidAudience = builder.Configuration["JWT:Audience"],
-		IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(key ?? throw new InvalidOperationException("JWT:Key not found")))
-	};
-});
-
-builder.Services.AddScoped<AppDbContext>();
+builder.Services.AddCustomServices(connectionString!);
+builder.Services.ConfigureHealthChecks(connectionString!);
+builder.Services.ConfigureSwagger();
+builder.Services.ConfigureIdentity();
+builder.Services.ConfigureJwtAuthentication(builder.Configuration);
 
 var app = builder.Build();
 
-using var scope = app.Services.CreateScope();
-var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-await dbContext.Database.MigrateAsync();
-
-app.UseSwagger(c =>
+if (app.Environment.IsDevelopment())
 {
-  c.PreSerializeFilters.Add((swagger, httpReq) =>
-  {
-    swagger.Servers = new List<OpenApiServer> { new OpenApiServer { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}/{httpReq.Headers["X-Forwarded-Prefix"]}" } };
-  });
-});
+	app.UseDatabaseMigration();
+}
 
-app.UseSwaggerUI(c => {
-	c.SwaggerEndpoint("v1/swagger.json", "Auth API v1");
-});
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapGet("/", () => "Hello world");
+app.UseMiddlewareComponents();
 
 app.Run();
