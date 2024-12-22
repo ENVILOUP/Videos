3babdb6a7e186f8c24f05c08f771a4672a7ef68b
justTrueCodeWriter <dmjustcoder8@gmail.com>
2024-12-22 20:09:48 +0300

    upd reqs

--- Search/app/main.py
+++ Search/app/main.py
@@ -1,28 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from elasticsearch import Elasticsearch
 
 app = FastAPI(
     root_path="/search"
 )
 
-es = Elasticsearch("http://elasticsearch:9200")
 
 @app.post("/")
 async def search(client_query: str):
-    query_json = {
-            "query": {
-                "match": {
-                    "title": {
-                        "query": client_query.lower(),
-                        "fuzziness": "2"
-                    }
-                }
-            }
-    }
-
-    response = es.search(index="video", body=query_json)
 
     hits_list = []
 
