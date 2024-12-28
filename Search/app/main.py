b8dada08f51ae119bed9e67bfe878927f3bb84dd
justTrueCodeWriter <dmjustcoder8@gmail.com>
2024-12-28 19:56:34 +0300

    add pagination and random sample for mock search service

--- Search/app/main.py
+++ Search/app/main.py
@@ -1,18 +1,19 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Query
+import random
+import config
 
 app = FastAPI(
     root_path="/search"
 )
 
 
-@app.post("/")
-async def search(client_query: str):
+@app.post("/find")
+async def search(page: int = Query(1, ge=1), size: int = Query(10, gt=0), query: str = Query("")):
 
-    hits_list = []
+    start_index = (page - 1) * size
+    end_index = start_index + size
+    paginated_ids = config.enviloup_ids[start_index:end_index]
 
-    for hit in response['hits']['hits']:
-        hits_list.append(hit['_source'])
+    random_ids = random.sample(paginated_ids, size-1)
 
-    return hits_list 
+    return random_ids 
