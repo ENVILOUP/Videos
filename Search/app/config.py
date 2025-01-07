63e40a839c209e161b83e3eb055ba83a83b75dd0
Dmitry <81821203+justTrueCodeWriter@users.noreply.github.com>
2025-01-07 19:34:06 +0300

    [DEV-24] Added fuzzy search with elastic (#4)
    * [DEV-24] Added simple fuzzy search with elastic
    
    * switched from pip to poetry && added nginx confs && added requirements.txt
    
    * Refactor configs and refactor elastic
    
    * Add shared network
    
    * upd reqs
    
    * add pagination and random sample for mock search service
    
    * [DEV-24] Refactor mock search
    
    * [DEV-24] Refactor configs
    
    * [DEV-24] Refactor and add README.md
    
    * [DEV-24] Refactor
    
    * [DEV-24] Refactor

---
+++ Search/app/config.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from dataclasses import dataclass
+from os import getenv
+
+
+def getenv_bool(key: str, default: bool) -> bool:
+    value = getenv(key)
+    if not value:
+        return default
+    if value.lower() == 'true':
+       return True
+    return False 
+
+
+@dataclass
+class Config:
+    debug: bool = getenv_bool('DEBUG', False)
+
+config = Config()
---
+++ Search/app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from app.views import router as search_router
+from app.config import config
+
+app = FastAPI(
+    title="Search Service",
+    debug=config.debug
+)
+
+class BaseMessage(BaseModel):
+    status: int
+    message: str
+
+@app.get("/")
+async def health_check():
+    return BaseMessage(status=200, message="Ok") 
+
+app.include_router(search_router, prefix="/search")
