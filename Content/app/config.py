a44625f90cfa6642ad14dcb824dcf4e3580f7590
Konstantin Serezhkin <konstantin.serez@gmail.com>
2025-01-13 12:31:00 +0300

    [DEV-36] New mechanism for applying migrations in Content service (#8)

--- Content/app/config.py
+++ Content/app/config.py
@@ -14,8 +14,8 @@ def getenv_bool(key: str, default: bool) -> bool:
 @dataclass(frozen=True)
 class Config:
     debug: bool = getenv_bool('DEBUG', True)
-    database: str = getenv('DATABASE', 'postgres://postgres:postgres@pg:5432/postgres')
-    cdn_base_url: str = getenv('CDN_BASE_URL', 'http://cdn.enviloup.localhost')
+    database: str = getenv('DATABASE', 'postgres://postgres:postgres@pg:5432/contentdb')
+    cdn_base_url: str = getenv('CDN_BASE_URL', 'http://cdn.enviloup.localhost')     
 
 
 config = Config()
