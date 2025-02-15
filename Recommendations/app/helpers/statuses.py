5da9858f4ab5cead54e165ae4c4ed94f29d6ee2d
jojopko <konstantin.serez@gmail.com>
2025-02-15 22:41:24 +0300

    Added a common template for successful responses and errors for recommendations

---
+++ Recommendations/app/helpers/statuses.py
@@ -0,0 +1,6 @@
+from enum import IntEnum
+
+
+class StatusCodes(IntEnum):
+    OK = 1000
+    VALIDATION_ERROR = 1001
--- Recommendations/app/main.py
+++ Recommendations/app/main.py
@@ -1,10 +1,16 @@
 from typing import Annotated
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import redis.asyncio as aioredis
 
 from app.routers import videos
 from app.dependencies import get_redis
+from app.helpers.exceptions import BaseAppException
+from app.helpers.schemas import ErrorResponse
+from app.helpers.statuses import StatusCodes
+from app.helpers.swagger import RESPONSES_TYPES_DOC
 
 
 class BaseMessage(BaseModel):
@@ -13,8 +19,8 @@ class BaseMessage(BaseModel):
 
 
 app = FastAPI(
-    title='Recommendations service',    
-    redirect_slashes=False
+    title='Recommendations service',
+    responses=RESPONSES_TYPES_DOC
 )
 
 
@@ -24,3 +30,23 @@ async def health_check(redis: Annotated[aioredis.Redis, Depends(get_redis)]):
 
 
 app.include_router(videos.router, prefix='/api/v1/videos')
+
+
+@app.exception_handler(BaseAppException)
+async def base_application_exception_handler(request: Request, exc: BaseAppException):
+    return JSONResponse(
+        status_code=exc.http_status_code,
+        content=ErrorResponse(
+            status_code=exc.app_status_code
+        ).model_dump()
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content=ErrorResponse(
+            status_code=StatusCodes.VALIDATION_ERROR
+        ).model_dump()
+    )
