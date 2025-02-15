14c3cd9f8223cd6118415216d204e4d367bc6ce5
jojopko <konstantin.serez@gmail.com>
2025-02-15 21:36:47 +0300

    Added a common template for successful responses and errors

--- Content/app/main.py
+++ Content/app/main.py
@@ -1,6 +1,12 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
+from app.helpers.swagger import RESPONSES_TYPES_DOC
+from app.helpers.exceptions import BaseAppException
+from app.helpers.schemas import ErrorResponse
+from app.helpers.statuses import StatusCodes
 from app.utils import try_init_kafka_connect
 from app.config import config
 from app.videos.views import router as videos_router
@@ -16,9 +22,31 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(
     title='Content Service',
-    lifespan=lifespan, 
-    debug=config.debug
+    lifespan=lifespan,
+    debug=config.debug,
+    responses=RESPONSES_TYPES_DOC
 )
 
 app.include_router(root_router, prefix="")
 app.include_router(videos_router, prefix='/videos')
+
+
+# TODO: to be moved to a separate module (DEV-46)
+@app.exception_handler(BaseAppException)
+async def base_application_exception_handler(request: Request, exc: BaseAppException):
+    return JSONResponse(
+        status_code=exc.http_status_code,
+        content=ErrorResponse(
+            status_code=exc.app_status_code
+        ).model_dump()
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content=ErrorResponse(
+            status_code=StatusCodes.VALIDATION_ERROR
+        ).model_dump()
+    )
--- Content/app/videos/views.py
+++ Content/app/videos/views.py
@@ -1,12 +1,16 @@
 import logging
-from typing import List, Optional, Annotated
+from typing import List, Annotated
 from uuid import UUID
 
 from fastapi import APIRouter, Depends
 from asyncpg import Connection
 
+from app.videos.models import VideoTag
+from app.helpers.schemas import SuccessResponse
+from app.helpers.statuses import StatusCodes
+from app.helpers.exceptions import DeprecatedException, NotFoundException
+from app.videos.schemas import VideoCreationModel, VideoModel, VideoTagModel
 from app.dependencies import get_connection
-from app.videos.models import Video, VideoTag
 from app.videos.repositories import VideosRespository, VideosTagsRespository
 
 
@@ -15,37 +19,88 @@ logger = logging.getLogger('uvicorn.error')
 router = APIRouter()
 
 
-@router.get("/{uuid}")
-async def get_video(uuid: UUID, db: Annotated[Connection, Depends(get_connection)]) -> Optional[Video]:
+@router.get(
+    path="/{uuid}",
+    response_model=SuccessResponse[VideoModel]
+)
+async def get_video(
+    uuid: UUID,
+    db: Annotated[Connection, Depends(get_connection)]
+):
     async with db as connection:
         video = await VideosRespository.get_video_by_uuid(connection, uuid)
-        return video
+
+    if not video:
+        raise NotFoundException(StatusCodes.NOT_FOUND)
+
+    video = video.model_dump()  # TODO: refactor database model in DEV-46
+    return {
+        'status_code': StatusCodes.OK,
+        'data': VideoModel(**video)
+    }
 
 
-@router.get("/")
-async def get_videos_list(videos_uuids: List[UUID], db: Annotated[Connection, Depends(get_connection)]) -> List[Video]:
+@router.post(
+    path="/get-bulk/",
+    response_model=SuccessResponse[List[VideoModel]]
+)
+async def get_videos_bulk(
+    db: Annotated[Connection, Depends(get_connection)],
+    video_uuids: List[UUID],
+):
     async with db as conn:
-        videos = await VideosRespository.get_videos_by_uuid_in(conn, videos_uuids)
-        return videos
+        videos = await VideosRespository.get_videos_by_uuid_in(conn, video_uuids)
+
+    # TODO: refactor database model in DEV-46
+    videos = [video.model_dump() for video in videos]
+    return {
+        'status_code': StatusCodes.OK,
+        'data': [VideoModel(**video) for video in videos]
+    }
 
 
-@router.post("/")
-async def update_video(video: Video, db: Annotated[Connection, Depends(get_connection)]) -> Optional[UUID]:
+@router.post(
+    path="/",
+    response_model=SuccessResponse[VideoModel]
+)
+async def update_video(
+    video: VideoCreationModel,
+    db: Annotated[Connection, Depends(get_connection)]
+):
+    raise DeprecatedException()  # TODO: refactor this method in DEV-46
+
+
+@router.get(
+    path='/{video_uuid}/tags/',
+    response_model=SuccessResponse[List[VideoTagModel]]
+)
+async def get_tags_for_video(
+    video_uuid: UUID,
+    db: Annotated[Connection, Depends(get_connection)]
+):
     async with db as conn:
-        uuid = await VideosRespository.add_video(conn, video)
-        return uuid
+        videos_tags = await VideosTagsRespository.get_tags_for_video(conn, video_uuid)
+        
+    return {
+        'status_code': StatusCodes.TAGS_ADDED,
+        'data': [VideoTagModel(**videos_tag.model_dump()) for videos_tag in videos_tags]
+    }
 
 
-@router.get('/{uuid}/tags/')
-async def get_tags_for_video(uuid: UUID, db: Annotated[Connection, Depends(get_connection)]) -> List[VideoTag]:
+@router.post(
+    path='/{video_uuid}/tags/',
+    response_model=SuccessResponse[List[VideoTagModel]]
+)
+async def add_tags_to_video(
+    video_uuid: UUID,
+    tags: List[str],
+    db: Annotated[Connection, Depends(get_connection)]
+):
     async with db as conn:
-        videos_tags = await VideosTagsRespository.get_tags_for_video(conn, uuid)
-        return videos_tags
-
-
-@router.post('/{uuid}/tags/')
-async def add_tags_for_video(uuid: UUID, tags: List[str], db: Annotated[Connection, Depends(get_connection)]) -> List[VideoTag]:
-    async with db as conn:
-        videos_tags = [VideoTag(video_uuid=uuid, tag=tag) for tag in tags]
+        videos_tags = [VideoTag(video_uuid=video_uuid, tag=tag) for tag in tags]  # TODO refactor this in DEV-46
         videos_tags = await VideosTagsRespository.add_tags_to_video(conn, videos_tags)
-        return videos_tags
+    
+    return {
+        'status_code': StatusCodes.TAGS_ADDED,
+        'data': [VideoTagModel(**videos_tag.model_dump()) for videos_tag in videos_tags]
+    }
