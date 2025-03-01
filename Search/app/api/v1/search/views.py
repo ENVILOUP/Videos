d369f2d412edf52bc0b08e0963246a6d347da26f
justTrueCodeWriter <dmjustcoder8@gmail.com>
2025-03-01 21:33:33 +0300

    [DEV-43] Update search endpoint

--- Search/app/api/v1/search/views.py
+++ Search/app/api/v1/search/views.py
@@ -18,8 +18,8 @@ router = APIRouter(
 )
 
 
-@router.get(path="/", response_model=SuccessResponse[SearchResponseModel])
-async def search(
+@router.get(path="/videos/", response_model=SuccessResponse[SearchResponseModel])
+async def search_videos(
     page: Annotated[int, Query(ge=1)] = 1,
     size: Annotated[int, Query(ge=1, le=50)] = 10,
     query: Annotated[str, Query(min_length=1)] = ...,
