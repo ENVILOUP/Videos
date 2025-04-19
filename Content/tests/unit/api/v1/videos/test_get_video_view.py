3094ecbee22afbc3f9ae18a2fc3f958101d1ed87
jojopko <konstantin.serez@gmail.com>
2025-04-19 10:10:57 +0300

    Дописал тесты ручки

--- Content/tests/unit/api/v1/videos/test_get_video_view.py
+++ Content/tests/unit/api/v1/videos/test_get_video_view.py
@@ -12,7 +12,7 @@ from app.main import app
 client = TestClient(app)
 
 
-class TestVideosViewsV1:
+class TestGetVideoViewV1:
 
     @staticmethod
     async def mock_get_video_by_uuid_use_case() -> GetVideoByUUIDUseCase:
@@ -32,8 +32,15 @@ class TestVideosViewsV1:
             get_video_by_uuid=MockGetVideoByUUIDPort
         )
 
+    @staticmethod
+    async def mock_get_video_by_uuid_use_case_not_found() -> GetVideoByUUIDUseCase:
+        MockGetVideoByUUIDPort = Mock(spec=GetVideoByUUIDPort)
+        MockGetVideoByUUIDPort.get_video_by_uuid = AsyncMock(return_value=None)
+        return GetVideoByUUIDUseCase(
+            get_video_by_uuid=MockGetVideoByUUIDPort
+        )
 
-    def test_get_video_by_uuid(self):
+    def test_get_video_by_uuid_happy_path(self):
         """
         Test the retrieval of a video by its UUID.
         """
@@ -44,10 +51,41 @@ class TestVideosViewsV1:
         
         assert response.status_code == 200
         assert response.json()["status_code"] == 1000
+        assert response.json()["success"] == True
         assert "data" in response.json()
         assert response.json()["data"]["video_uuid"] == valid_uuid
 
         app.dependency_overrides.clear()
+    
+    def test_get_video_by_uuid_not_found(self):
+        """
+        Test the retrieval of a video by UUID that does not exist.
+        """
+        not_found_uuid = "123e4567-e89b-12d3-a456-426614174000"
+        app.dependency_overrides[get_video_by_uuid_use_case] = self.mock_get_video_by_uuid_use_case_not_found
+
+        response = client.get(f"/api/v1/videos/{not_found_uuid}")
+        
+        assert response.status_code == 404
+        assert response.json()["success"] == False
+        assert response.json()["status_code"] == 1002
+
+        app.dependency_overrides.clear()
+    
+    def test_get_video_by_uuid_invalid_uuid(self):
+        """
+        Test the retrieval of a video by an invalid UUID.
+        """
+        invalid_uuid = "invalid-uuid"
+
+        app.dependency_overrides[get_video_by_uuid_use_case] = self.mock_get_video_by_uuid_use_case
+        
+        response = client.get(f"/api/v1/videos/{invalid_uuid}")
+        
+        assert response.status_code == 422
+        assert response.json()["success"] == False
+        assert response.json()["status_code"] == 1001
+
 
 
 
